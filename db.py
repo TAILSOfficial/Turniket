1d952ec056ec5361c9ef599ee165b761fba787e4
unknown <Jenia20050605@gmail.com>
2022-05-23 19:56:31 +0600

    tails
    update many

--- db.py
+++ db.py
@@ -5,80 +5,72 @@ import sqlite3
 import cv2
 import os
 
-db = sqlite3.connect('main.db')
-cursor = db.cursor()
-collages = {}
-
 def pathgen():
-	i = 0
-	while True:
-		yield i
-		i += 1
+    i = 0
+    while True:
+        yield i
+        i += 1
 gener = pathgen()
 for i in os.listdir('imgs'):
-	next(gener)
+    next(gener)
 
 class Collage_manager:
-	def __init__(self, collage_name: str):
-		global cursor, db
+    def __init__(self, collage_name: str):
+        self.db = sqlite3.connect('main.db')
+        self.cursor = self.db.cursor()
 
-		self.collage_name = collage_name
-		cursor.execute('''CREATE TABLE IF NOT EXISTS {0}_members (
-							fio CHAR(255),
-							grup CHAR(255)
-						);
-						CREATE TABLE IF NOT EXISTS {0}_info (
-							fio CHAR(255),
-							grup CHAR(255),
-							path_to_img TEXT,
-							dute DATETIME
-						);
-						CREATE TABLE IF NOT EXISTS {0}_cash (
-							fio CHAR(255),
-							grup CHAR(255),
-							frame BLOB,
-							dute DATETIME
-						)'''.format(self.collage_name))
-		db.commit()
+        self.collage_name = collage_name
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS {0}_members (
+                            fio CHAR(255),
+                            grup CHAR(255)
+                        )'''.format(self.collage_name))
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS {0}_info (
+                            fio CHAR(255),
+                            grup CHAR(255),
+                            path_to_img TEXT,
+                            dute DATETIME
+                        )'''.format(self.collage_name))
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS {0}_cash (
+                            fio CHAR(255),
+                            grup CHAR(255),
+                            frame BLOB,
+                            dute DATETIME
+                        )'''.format(self.collage_name))
+        self.db.commit()
 
-	def add_user(self, fio, grup):
-		global cursor, db
+    def add_user(self, fio, grup):
+        self.cursor.execute('INSERT INTO {0}_members VALUES (?, ?)'.format(self.collage_name), (fio, grup))
+        self.db.commit()
 
-		cursor.execute('INSERT INTO {0}_members VALUES (?, ?)'.format(self.collage_name), (fio, grup))
-		db.commit()
+    def add_cash(self, fio, grup, frame, date):
+        self.cursor.execute('INSERT INTO {0}_cash VALUES (?, ?, ?, ?)'.format(self.collage_name), (fio, grup, frame, date))
+        self.db.commit()
 
-	def add_cash(self, fio, grup, frame, date):
-		global cursor, db
+    def found_data_in_db(self, data: str):
+        for fio, grup in self.cursor.execute('SELECT * FROM {0}_members'.format(self.collage_name)).fetchall():
+            f, g = data.split(', ')
+            if fio == f and g == grup:
+                return True
+        return False
 
-		cursor.execute('INSERT INTO {0}_cash VALUES (?, ?, ?, ?)'.format(self.collage_name), (fio, grup, frame, date))
+    def pop_cash(self):
+        cash = self.cursor.execute('SELECT * FROM {0}_cash'.format(self.collage_name)).fetchall()
+        self.cursor.execute('DELETE FROM {0}_cash'.format(self.collage_name))
+        self.db.commit()
 
-	def pop_cash(self):
-		global cursor, db
+        for data in cash:
+            yield data
 
-		cash = cursor.execute('SELECT * FROM {0}_cash'.format(self.collage_name)).fetchall()
-		cursor.execute('DELETE FROM {0}_cash'.format(self.collage_name))
-		db.commit()
+    def add_row(self, fio, grup, frame, date):
+        if frame:
+            path = os.path.join(os.getcwd(), f'imgs/{str(next(gener))}.jpg')
+            cv2.imwrite(path, frame)
+        else:
+            path = 'None'
 
-		for data in cash:
-			yield data
-
-	def add_row(self, fio, grup, frame, date):
-		global cursor, db
-
-		if frame:
-			path = os.path.join(os.getcwd(), f'imgs/{str(next(gener))}.jpg')
-			cv2.imwrite(path, frame)
-		else:
-			path = 'None'
-
-		cursor.execute('INSERT INTO {0}_info VALUES (?, ?, ?, ?)'.format(self.collage_name), (fio, grup, path, date))
-		db.commit()
+        self.cursor.execute('INSERT INTO {0}_info VALUES (?, ?, ?, ?)'.format(self.collage_name), (fio, grup, path, date))
+        self.db.commit()
 
 
 def init_db(collage_name: str):
-	global collages
-
-	if collage_name not in collages.keys():
-		collages.update({collage_name: Collage_manager(collage_name)})
-
-	return collages[collage_name]
+    return Collage_manager(collage_name)
--- turniket.py
+++ turniket.py
@@ -1,110 +1,139 @@
 from loguru import logger
 from rich import print
 
+import RPi.GPIO as GPIO
 import threading
 import datetime
 import socket
 import pickle
 import struct
+import atexit
+import time
 import cv2
 import db
 
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.settimeout(5)
 payload_size = struct.calcsize("Q")
 detector = cv2.QRCodeDetector()
 cam0 = cv2.VideoCapture(0)
 cam1 = cv2.VideoCapture(1)
 MY_COLLAGE = 'KTSK'
 db.init_db(MY_COLLAGE)
+NOT_STOP = True
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.OUT)
+atexit.register(GPIO.cleanup)
+
+def go_away():
+    GPIO.output(23, GPIO.HIGH)
+    time.sleep(3)
+    GPIO.output(23, GPIO.LOW)
 
 def send_data(data: str, cast: tuple[bool, "numpy.ndarray"], date = datetime.datetime.now()):
-	global net_success
+    global net_success
 
-	if cast[0]:
-		frame = pickle.dumps(cast[1])
-	else:
-		frame = None
+    if cast[0]:
+        frame = pickle.dumps(cast[1])
+    else:
+        frame = None
 
-	if net_success:
-		sig = pickle.dumps((*data.split(', '), frame, date))
-		client.send(struct.pack('Q', len(sig)) + sig)
-	else:
-		db.init_db(MY_COLLAGE).add_cash(*data.split(', '), frame, date)
+    if net_success:
+        sig = pickle.dumps((*data.split(', '), frame, date))
+        client.send(struct.pack('Q', len(sig)) + sig)
+    else:
+        db.init_db(MY_COLLAGE).add_cash(*data.split(', '), frame, date)
 
 def add_new():
-	global net_success, client, payload_size
+    global net_success, client, payload_size, NOT_STOP
 
-	if net_success:
-		while True:
-			data = client.recv(4 * 1024)
-			while len(data) < payload_size:
-				packet = client.recv(4 * 1024) # 4K
-				if not packet:
-					break
-				data += packet
-			packed_msg_size = data[:payload_size]
-			data = data[payload_size:]
-			msg_size = struct.unpack("Q", packed_msg_size)[0]
-			while len(data) < msg_size:
-				data += client.recv(4 * 1024)
-			frame_data = data[:msg_size]
-			data = data[msg_size:]
-			frame = pickle.loads(frame_data)
-			db.init_db(MY_COLLAGE).add_user(*frame)
+    if net_success:
+        while NOT_STOP:
+            data = client.recv(4 * 1024)
+            while len(data) < payload_size:
+                packet = client.recv(4 * 1024) # 4K
+                if not packet:
+                    break
+                data += packet
+            packed_msg_size = data[:payload_size]
+            data = data[payload_size:]
+            msg_size = struct.unpack("Q", packed_msg_size)[0]
+            while len(data) < msg_size:
+                data += client.recv(4 * 1024)
+            frame_data = data[:msg_size]
+            data = data[msg_size:]
+            frame = pickle.loads(frame_data)
+            db.init_db(MY_COLLAGE).add_user(*frame)
 
 try:
-	client.connect(('', 5723))
-	msg = pickle.dumps('SET_COLLAGE KTSK')
-	client.send(struct.pack('Q', len(msg)) + msg)
-	net_success = True
+    client.connect(('192.168.0.127', 5723))
+    msg = pickle.dumps('SET_COLLAGE KTSK')
+    client.send(struct.pack('Q', len(msg)) + msg)
+    net_success = True
 
-	for msg in db.init_db(MY_COLLAGE).pop_cash():
-		_ = pickle.loads(msg[2])
-		a = ', '.join((msg[0], msg[1]))
-		b = (bool(_), _)
-		send_data(a, b, date = msg[3])
+    for msg in db.init_db(MY_COLLAGE).pop_cash():
+        _ = pickle.loads(msg[2])
+        a = ', '.join((msg[0], msg[1]))
+        b = (bool(_), _)
+        send_data(a, b, date = msg[3])
 
-	threading.Thread(target = add_new).start()
+    threading.Thread(target = add_new).start()
 
 except Exception as e:
-	logger.exception(e)
-	net_success = False
+    logger.exception(e)
+    net_success = False
 
-while True:
+def mega_qr():
+    global img, NOT_STOP, detector
+    
+    my_db = db.init_db(MY_COLLAGE)
+    
+    while NOT_STOP:
+        # t = time.time()
+        data, bbox, _ = detector.detectAndDecode(img)
+        # print(time.time() - t)
+    
+        if (bbox is not None) and data != '':
+            for i in range(len(bbox)):
+                a = tuple([int(k) for k in bbox[i][0]])
+                b = tuple([int(k) for k in bbox[(i+1) % len(bbox)][1]])
+                cv2.line(img, a, b, color = (255, 0, 0), thickness = 2)
+
+            cv2.putText(img, data, (int(bbox[0][0][0]), int(bbox[0][0][1]) - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                        1, (0, 255, 0), 2)
+
+            if my_db.found_data_in_db(data):
+                threading.Thread(target = send_data, args = (data, cam1.read())).start()
+                go_away()
+
+cam_success, img = cam0.read()
+if cam_success:
+    threading.Thread(target = mega_qr).start()
+
+while cam_success:
     cam_success, img = cam0.read()
 
     if not cam_success:
-    	break
-
-    data, bbox, _ = detector.detectAndDecode(img)
-    
-    if (bbox is not None):
-        for i in range(len(bbox)):
-        	print(bbox)
-            a = tuple([int(k) for k in bbox[i][0]])
-            b = tuple([int(k) for k in bbox[(i+1) % len(bbox)][1]])
-            cv2.line(img, a, b, color = (255, 0, 0), thickness = 2)
-
-        cv2.putText(img, data, (int(bbox[0][0][0]), int(bbox[0][0][1]) - 10), cv2.FONT_HERSHEY_SIMPLEX,
-                    1, (0, 255, 0), 2)
-
-        if db.found_data_in_db(data):
-        	threading.Thread(target = send_data, args = (data, cam1.read())).start()
-        	# put it
+        break
 
     cv2.imshow("code detector", img)
     if(cv2.waitKey(1) == ord("q")):
+        NOT_STOP = False
         break
 
 if not cam_success:
-	img = cv2.imread('Error.png')
-	while True:
-		cv2.imshow("Error!", img)
+    cv2.destroyAllWindows()
+    img = cv2.imread('error.png')
+    while True:
+        cv2.imshow("Error!", img)
 
-		if cv2.waitKey(1) == ord("q"):
-    		break
+        if cv2.waitKey(1) == ord("q"):
+            break
 
 cam0.release()
 cam1.release()
+NOT_STOP = False
+time.sleep(1)
 cv2.destroyAllWindows()
